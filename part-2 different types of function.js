f4f2bc156300a6a55505b347667c74bbe13d88e0
Abhishek R <abhibdk2004@gmail.com>
2026-06-03 02:22:17 +0530

    4 different types of function

---
+++ part-2 different types of function.js
@@ -0,0 +1,30 @@
+let num1 = 100;
+let num2 = 300;
+
+// no i/p parameter and no return value
+function add1(){
+    console.log(num1+num2);
+}
+add1()
+
+// no i/p parameter with return value
+function add2(){
+    return num1+num2;
+}
+let sum1 = add2()
+console.log(sum1);
+
+// with i/p value no return type
+function add3(x,y){
+    console.log(x+y);
+}
+add3(num1,num2)
+
+
+function add4(x,y){
+    return x+y;
+}
+let sum2 = add4(num1,num2)
+console.log(sum2);
+
+
